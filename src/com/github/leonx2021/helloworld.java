d4a331251b9aa00161b488cf98dbaa00a213e1f5
leonx <leonx@outlook.com>
2022-08-12 18:25:48 +0800

    Changes 2

--- src/com/github/leonx2021/helloworld.java
+++ src/com/github/leonx2021/helloworld.java
@@ -3,5 +3,6 @@ package com.github.leonx2021;
 public class helloworld {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("git test");
     }
 }
